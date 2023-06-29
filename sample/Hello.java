eb7ed36d53e3984fefa8f2736b63106341b84e70
Lekshmi.S.Nair <136094821+lekshmigit-creater@users.noreply.github.com>
2023-06-29 16:09:34 +0530

    add my files to repository

---
+++ sample/Hello.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Hello {
+	void display(String a)
+	{
+		System.out.println(a);	
+	}
+	public static void main(String args[])
+	{
+	Hello ob=new Hello();
+	ob.display("hello world");
+	}
+
+}
+
